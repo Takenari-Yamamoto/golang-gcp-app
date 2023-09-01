63884959c8ca4bed7012ad2c94e9cdc73afbd101
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2023-09-02 08:40:21 +0900

    fix

--- services/app-001/backend/cmd/server/main.go
+++ services/app-001/backend/cmd/server/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!!!")
+		fmt.Fprintf(w, "Hello World Desu!!!")
 	})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
